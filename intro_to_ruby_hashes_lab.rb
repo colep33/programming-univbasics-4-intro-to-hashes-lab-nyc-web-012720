279513897556e28575b52e3dd4eb4ba3353013ef
Cole Arthur Parham <coleparham@gmail.com>
2019-12-30 20:44:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,10 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  read_from_hash = {
+    hash => key
+  }
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
